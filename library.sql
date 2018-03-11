b788932e35ce9d07d067d0fca3387e671b66ac4f
Alyssa <alyssarcoops@gmail.com>
2018-03-11 17:04:34 -0400

    added inserts

--- library.sql
+++ library.sql
@@ -19,11 +19,11 @@ CONSTRAINT Staff_PK PRIMARY KEY (Staff_Id)
 
 CREATE TABLE BOOK_t
 (
-	Book_ID			VARCHAR(20),
+	Book_ID			VARCHAR(50),
 	Author			VARCHAR(20),
-	ISBN 			VARCHAR(20),
+	ISBN 			INT(14),
 	Catalogued		VARCHAR(20),
-	Holds			VARCHAR(20),
+	Holds			INT(3),
 CONSTRAINT Book_PK PRIMARY KEY (Book_ID)
 );
 
@@ -32,8 +32,8 @@ CREATE TABLE TRANSACTION_t
 	Transaction_ID		VARCHAR(20),
 	Item_Out			VARCHAR(20),
 	Who_Checked_Out 	VARCHAR(20),
-	When_Checked_Out 	VARCHAR(20),
-	Return_Date 		VARCHAR(20),
+	When_Checked_Out 	DATE,
+	Return_Date 		DATE),
 CONSTRAINT Transaction_PK PRIMARY KEY (Transaction_ID)
 );
 
@@ -42,17 +42,17 @@ CREATE TABLE EVENT_t
 	event_Id 				VARCHAR(20),
 	Exhibits_On_Display		VARCHAR(20),
 	Owner_Of_Exhibit 		VARCHAR(20),
-	Start_Date				VARCHAR(20),
-	End_Date 				VARCHAR(20),
+	Start_Date				DATE,
+	End_Date 				DATE,
 	Owner_Email 			VARCHAR(20),
-	Phone_Number			INT(8),
+	Phone_Number			INT(10),
 CONSTRAINT Event_PK PRIMARY KEY (event_Id)
 );
 
 CREATE TABLE MEDIA_t
 (
 	Media_Id 		VARCHAR(20),
-	Media_Name		VARCHAR(30),
+	Media_Type	VARCHAR(30),
 	Holds			CHAR(30),
 CONSTRAINT Media_PK PRIMARY KEY (Media_Id)
 );
@@ -61,7 +61,6 @@ CREATE TABLE STUDYROOM_t
 (
 	study_Room_Id	VARCHAR(5),
 	Student_Id 		VARCHAR(9),
-	room_number	VARCHAR(9),
 	timeRequested		TIME(),
 CONSTRAINT Study_Room_PK PRIMARY KEY (study_Room_Id),
 CONSTRAINT Study_Room_FK1 FOREIGN KEY (Student_Id) REFERENCES STUDENT_t(Student_Id)
@@ -73,7 +72,88 @@ CREATE TABLE STUDENT_t (
 	Password 		VARCHAR(20),
 	Student_Email	VARCHAR(20),
 	Item_Borrowed	VARCHAR(30),
-	Return_Date		VARCHAR(20),
+	Return_Date		DATE,
 	Student_Name	CHAR(30),
 CONSTRAINT Student_PK PRIMARY KEY (Student_Id)
 );
+
+INSERT INTO STUDENT_t
+VALUES ('W00875435', 'coynej', 'bubbles', 'coynej@wit.edu', 'Ready Player One', '2018-05-24', 'James Coyne');
+INSERT INTO STUDENT_t
+VALUES ('W00123456', 'toubeaus', 'bread', 'toubeaus@wit.edu', 'Saga Vol 1', '2018-04-01', 'Shawnathan Toubeau');
+INSERT INTO STUDENT_t
+VALUES ('W00789654', 'beaucagej', 'pizza', 'beaucagej@wit.edu', 'Eye of the World', '2018-05-03', 'Jeffrey Beaucage');
+INSERT INTO STUDENT_t
+VALUES ('W00678876', 'seltenreichs', 'notEvil', 'seltenreichs@wit.edu', 'Black Panther Comic', '2019-03-23', 'Sophia Seltenreich');
+INSERT INTO STUDENT_t
+VALUES ('W00456321', 'kylev', 'coastModern', 'kylev@wit.edu', 'Rogue One', '2019-03-13', 'Vicky Kyle');
+INSERT INTO STUDENT_t
+VALUES ('W00124567', 'petersena', 'darkchocolate', 'petersena@wit.edu', 'Cracking the Coding Interview', '2018-05-21', 'Abigail Petersen');
+
+INSERT INTO BOOK_t
+VALUES ('Ready Player One', 'Ernest Cline', 8297827818301, 'Book', 0);
+INSERT INTO BOOK_t
+VALUES ('Saga Vol 1', 'John Cena', 8959120715759, 'Book', 3);
+INSERT INTO BOOK_t
+VALUES ('Eye of the World', 'Robert Jordan', 7718800405691, 'Book', 3);
+INSERT INTO BOOK_t
+VALUES ('Black Panther Comic', 'Stan Lee', 6692457568506, 'Book', 1);
+INSERT INTO BOOK_t
+VALUES ('Rogue One', 'George Lucas', 9757089502679, 'Book', 0);
+INSERT INTO BOOK_t
+VALUES ('Cracking the Coding Interview', 'Gayle G', 7079559901159, 'Book', 0);
+
+INSERT INTO STUDYROOM_t
+VALUES ('1', 'W00875435', 3:00);
+INSERT INTO STUDYROOM_t
+VALUES ('2', 'W00123456', 4:30);
+INSERT INTO STUDYROOM_t
+VALUES ('3', 'W00789654', 7:45);
+INSERT INTO STUDYROOM_t
+VALUES ('4', 'W00678876', 8:15);
+INSERT INTO STUDYROOM_t
+VALUES ('5', 'W00456321', 1:15);
+INSERT INTO STUDYROOM_t
+VALUES ('6', 'W00124567', 2:30);
+
+INSERT INTO STAFF_t
+VALUES ('w00985436', 'sheamusp' 'password', 'sheamusp@wit.edu', 'librarian');
+INSERT INTO STAFF_t
+VALUES ('w00988754', 'hellenk' 'cheeseburgers', 'hellenk@wit.edu', 'help desk');
+INSERT INTO STAFF_t
+VALUES ('w00654709', 'mirandao' 'ilovebooks', 'mirandao@wit.edu', 'help desk');
+INSERT INTO STAFF_t
+VALUES ('w00985436', 'perryj' 'funbooks', 'perryj@wit.edu', 'librarian');
+INSERT INTO STAFF_t
+VALUES ('w00546436', 'crosbyc' 'mechanicalkeyboards', 'crosbyc@wit.edu', 'work study');
+
+INSERT INTO MEDIA_t
+VALUES ('Star Wars', 'Movie', 3);
+INSERT INTO MEDIA_t
+VALUES ('The Avengers', 'Movie', 0);
+INSERT INTO MEDIA_t
+VALUES ('Dark Side of the Moon', 'CD', 1);
+INSERT INTO MEDIA_t
+VALUES ('Entertainment', 'CD', 15);
+INSERT INTO MEDIA_t
+VALUES ('Scott Pilgrim vs The World', 'Movie', 0);
+INSERT INTO MEDIA_t
+VALUES ('After Laughter', 'CD', 7);
+
+INSERT INTO EVENT_t
+VALUES ('Virtual Fractality', 1, 'Ava Poppas', '1-MARCH-2018', '1-APRIL-2018', 'poppasa@wit.edu', '1234567899');
+INSERT INTO EVENT_t
+VALUES ('Career Fair', 1, 'Co-op Office', '20-MARCH-2018', '21-MARCH-2018', 'coopoffice@wit.edu', '2034506578');
+
+INSERT INTO TRANSACTION_t
+VALUES('svbx3619jf', 'Ready Player One', 'W00875435', '2018-05-24', '2018-05-24');
+INSERT INTO TRANSACTION_t
+VALUES('mm52ti67mb', 'Saga Vol 1', 'W00123456', '2018-04-16', '2018-05-10');
+INSERT INTO TRANSACTION_t
+VALUES('mn91g4xhh3', 'Eye of the World', 'W00789654', '2018-05-3', '2018-05-18');
+INSERT INTO TRANSACTION_t
+VALUES('9hygaqbf5h', 'Black Panther Comic', 'W00678876', '2018-03-7', '2018-03-21');
+INSERT INTO TRANSACTION_t
+VALUES('7rtg3kqqeq', 'Rogue One', 'W00456321', '2018-03-12', '2018-03-24');
+INSERT INTO TRANSACTION_t
+VALUES('nz3uz3kicb', 'Cracking the Coding Interview', 'W00124567', '2018-05-7', '2018-05-22');
